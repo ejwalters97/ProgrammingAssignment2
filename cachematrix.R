d824e7a0442f48ca950651e2d9c655b72977c0d9
Eric Walters <ejwalters97@gmail.com>
2015-03-21 17:57:45 -0400

    final

--- cachematrix.R
+++ cachematrix.R
@@ -36,19 +36,3 @@ cacheSolve <- function(x, ...) {
     x$setMatrix(inv)
     inv
 }
-
-test <- function(mat){
-  ## @mat: an invertible matrix
-  
-  temp <- makeCacheMatrix(mat)
-  
-  start.time <- Sys.time()
-  cacheSolve(temp)
-  dur <- Sys.time() - start.time
-  print(dur)
-  
-  start.time <- Sys.time()
-  cacheSolve(temp)
-  dur <- Sys.time() - start.time
-  print(dur)
-}
