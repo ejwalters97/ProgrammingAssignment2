c5d01175c564ef9823236a8ab4d831a03477cba0
Eric Walters <ejwalters97@gmail.com>
2015-03-21 17:57:16 -0400

    assignment with testing function to compare time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit 
+## to caching the inverse of a matrix rather than compute it repeatedly.
+## These functions cache the inverse of a given matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list that contains a function to set a 
+## a matrix, get a matrix, set an inverse, and get an inverse
+## this list is the input to cacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(m) {
+    x <<- m
+    inv <<- NULL
+  }
+  get <- function() x
+  setMatrix <- function(inverse) inv <<- inverse
+  getMatrix <- function() inv
+  list(set=set, get=get, setMatrix=setMatrix, getMatrix=getMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then the cachesolve should retrieve the inverse 
+## from the cache.
 
+## solves a matrix, or uses the cache if possible
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getMatrix()
+    if(!is.null(inv)) {
+      message("cached")
+      return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$setMatrix(inv)
+    inv
+}
+
+test <- function(mat){
+  ## @mat: an invertible matrix
+  
+  temp <- makeCacheMatrix(mat)
+  
+  start.time <- Sys.time()
+  cacheSolve(temp)
+  dur <- Sys.time() - start.time
+  print(dur)
+  
+  start.time <- Sys.time()
+  cacheSolve(temp)
+  dur <- Sys.time() - start.time
+  print(dur)
 }
